44c29f6f62d46008918b6bb68447efe079104946
Sébastien DEJEAN <sdejean@lca-pharma.com>
2019-11-18 14:46:54 +0100

    Convert to RPN

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,70 +7,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  currentNumber = '0';
+  currentNumber = '';
   firstOperand = null;
   operator = null;
   waitForSecondNumber = false;
+  stack = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public getNumber(v: string){
-    console.log(v);
-    if(this.waitForSecondNumber)
-    {
-      this.currentNumber = v;
-      this.waitForSecondNumber = false;
-    }else{
-      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
-
+  fakeArray(length: number): Array<any> {
+    if (length >= 0) {
+      return new Array(length);
     }
   }
 
+
+  public getNumber(v: string){
+    console.log('getNumber',v);
+    this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+  }
+
   getDecimal(){
+    console.log('getDecimal');
+
     if(!this.currentNumber.includes('.')){
         this.currentNumber += '.'; 
     }
   }
 
-  private doCalculation(op , secondOp){
-    switch (op){
-      case '+':
-      return this.firstOperand += secondOp; 
-      case '-': 
-      return this.firstOperand -= secondOp; 
-      case '*': 
-      return this.firstOperand *= secondOp; 
-      case '/': 
-      return this.firstOperand /= secondOp; 
-      case '=':
-      return secondOp;
-    }
-  }
+
   public getOperation(op: string){
-    console.log(op);
-
-    if(this.firstOperand === null){
-      this.firstOperand = Number(this.currentNumber);
-
-    }else if(this.operator){
-      const result = this.doCalculation(this.operator , Number(this.currentNumber))
-      this.currentNumber = String(result);
-      this.firstOperand = result;
+    console.log('getOperation',op);
+    
+    if (this.currentNumber!=='')
+    {
+      this.addPile(this.currentNumber);
     }
-    this.operator = op;
-    this.waitForSecondNumber = true;
 
-    console.log(this.firstOperand);
- 
+
+    if (this.stack.length>=2)
+    {
+      var o1 = this.stack.shift();
+      var o2 = this.stack.shift();
+
+      console.log('stack', Object.assign(this.stack));
+      console.log('o1,o2',o1,o2);
+
+      switch (op){
+        case '+':
+          this.stack.unshift(o2+o1); 
+          break;
+        case '-': 
+          this.stack.unshift(o2-o1); 
+          break;
+        case '*': 
+          this.stack.unshift(o2*o1); 
+          break;
+        case '/': 
+          this.stack.unshift(o2/o1); 
+          break;
+//        case '=':
+//        return secondOp;
+      }
+
+    }
+     
   }
 
   public clear(){
-    this.currentNumber = '0';
-    this.firstOperand = null;
-    this.operator = null;
-    this.waitForSecondNumber = false;
+    this.currentNumber = '';
+  }
+
+  public clearStack(){
+    this.currentNumber = '';
+    this.stack = [];
+  }
+
+  public addPile( number ){
+    if(number!=''){
+      this.stack.unshift( parseFloat(number) );
+      this.currentNumber='';
+    }
+    else {
+      var o = this.stack.shift();
+      if (o) {
+        this.stack.unshift( o );
+        this.stack.unshift( o );
+      }
+    }
   }
 }
